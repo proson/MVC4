3e4c65a2062cf969177ba4894691a1f73b7dd963
刘稳 <lw3039@17u.cn>
2014-02-28 09:53:06 +0800

    cc

--- MvcStudy/MvcStudy/Controllers/HomeController.cs
+++ MvcStudy/MvcStudy/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcStudy.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application1.";
 
             return View();
         }
